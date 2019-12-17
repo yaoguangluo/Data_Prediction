fcba78789ef85e8df73068206a613dcf93967aab
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2019-12-17 12:14:05 +0800

    增加基于簇重心来进行新坐标分类添加算法。20191217 罗瑶光

---
+++ src/org/tinos/deta/classification/PositionClasification.java
@@ -0,0 +1,40 @@
+package org.tinos.deta.classification;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import org.tinos.deta.cluster.Distance;
+import org.tinos.deta.cluster.Eclid;
+import org.tinos.deta.cluster.Position2D;
+//这个函数用于通过重心位移距离来进行坐标分类
+//思想：欧基里德, 罗瑶光
+//实现：罗瑶光
+public class PositionClasification{
+	public static Map<String, List<Position2D>> addNewPositionWithoutHeart(Map<String
+			, List<Position2D>> groups, Position2D position2D){
+		double shortestDistance = 0;
+		String predictionGroup = null;
+		Iterator<String> iterator= groups.keySet().iterator();
+		boolean isFirst= true;
+		while(iterator.hasNext()) {
+			String groupKey= iterator.next();
+			List<Position2D> group= groups.get(groupKey);
+			Position2D heart= Eclid.findHeartPosition2D(group);
+			double distance= Distance.getDistance2D(heart, position2D);
+			if(isFirst) {
+				isFirst= !isFirst;
+				shortestDistance= distance;
+			}
+			if(shortestDistance< distance) {
+				shortestDistance= distance;
+				predictionGroup= groupKey;
+			}
+		}
+		//添加坐标
+		if(null!= predictionGroup) {
+			List<Position2D> group= groups.get(predictionGroup);
+			group.add(position2D);
+			groups.put(predictionGroup, group);
+		}
+		return groups;	
+	}
+}
---
+++ src/org/tinos/deta/classification/ProbabilityClasification.java
@@ -0,0 +1,7 @@
+package org.tinos.deta.classification;
+//这个函数用于通过概率相似度来进行坐标分类
+//思想：贝叶斯，罗瑶光
+//实现：罗瑶光
+public class ProbabilityClasification{
+	
+}
