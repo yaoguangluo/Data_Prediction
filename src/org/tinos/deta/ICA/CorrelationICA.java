68cece1a184b435591f53c031af30f3de9a99293
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2019-12-18 14:08:31 +0800

    修正了下。 20191218 罗瑶光

--- src/org/tinos/deta/ICA/CorrelationICA.java
+++ src/org/tinos/deta/ICA/CorrelationICA.java
@@ -12,7 +12,7 @@ public class CorrelationICA{
 	public static double[] frequencyDownSplit(double[] originFrequency, double[] compareFrequency) {
 		double[] output= new double[originFrequency.length];
 		for(int i= 0; i< originFrequency.length; i++) {
-			output[i]= originFrequency[i]- compareFrequency[i]< 0? originFrequency[i]: 0;
+			output[i]= originFrequency[i]- compareFrequency[i]< 0? compareFrequency[i]: 0;
 		}
 		return output;	
 	}
