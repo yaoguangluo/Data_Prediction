2b77f9f8d631b2dcd961e563dbdfcd1e13f99457
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2019-12-18 14:36:22 +0800

    冗余去除 20191218 罗瑶光

--- src/org/tinos/deta/classification/ProbabilityClasification.java
+++ src/org/tinos/deta/classification/ProbabilityClasification.java
@@ -9,40 +9,6 @@ import org.tinos.deta.cluster.RatioMatrix;
 //思想：贝叶斯
 //实现：罗瑶光
 public class ProbabilityClasification{
-	public static boolean predictionResult(RatioMatrix input, List<Ratio> right, List<Ratio> error, double scale) {
-		double rightRightMean= 0;
-		double rightErrorMean= 0;
-		double errorRightMean= 0;
-		double errorErrorMean= 0;
-		//成功集
-		Iterator<Ratio> iteratorRight= right.iterator();
-		while(iteratorRight.hasNext()) {
-			Ratio ratio= iteratorRight.next();
-			rightRightMean+= ratio.getRightRatio();
-			rightErrorMean+= ratio.getErrorRatio();
-		}
-		rightRightMean= rightRightMean/ right.size();
-		rightErrorMean= rightErrorMean/ right.size();
-		//失败集
-		Iterator<Ratio> iteratorError= error.iterator();
-		while(iteratorError.hasNext()) {
-			Ratio ratio= iteratorError.next();
-			errorRightMean+= ratio.getRightRatio();
-			errorErrorMean+= ratio.getErrorRatio();
-		}
-		errorRightMean= errorRightMean/ error.size();
-		errorErrorMean= errorErrorMean/ error.size();
-		//决策轭
-		double predictionRightRight= input.getRightRightRatio()- rightRightMean;
-		double predictionRightError= input.getRightErrorRatio()- rightErrorMean;
-		double predictionErrorRight= input.getErrorRightRatio()- errorRightMean;
-		double predictionErrorError= input.getErrorErrorRatio()- errorErrorMean;
-		//迪摩根轭集
-		double pridictionRight= predictionRightRight+ predictionErrorError;
-		double pridictionError= predictionRightError+ predictionErrorRight;
-		return pridictionRight> pridictionError+ scale? true: false;	
-	}
-	
 	public static boolean predictionResult(RatioMatrix input, List<RatioMatrix> groups, double scale) {
 		double rightRightMean= 0;
 		double rightErrorMean= 0;
