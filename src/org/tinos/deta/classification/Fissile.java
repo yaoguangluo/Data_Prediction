84768895adb8bbf3771e0c1ade4a8edd626b0e35
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2019-12-19 15:38:47 +0800

    一种通过scale 距离来进行2维坐标团集合 融聚算法。 20191219 罗瑶光

--- src/org/tinos/deta/classification/Fissile.java
+++ src/org/tinos/deta/classification/Fissile.java
@@ -9,54 +9,55 @@ import org.tinos.deta.cluster.Eclid;
 import org.tinos.deta.cluster.Position2D;
 //task 20191219 daytime
 //通过scale 距离来进行 坐标团切裂。
-//Theory yaoguang.luo 20191219
+//Theory yaoguang.luo 20191219， 欧基里德
 //Application yaoguang.luo
 public class Fissile{
-	public static Map<Double, List<Position2D>> fissilePosition2D(List<Position2D> groups, double scale) {
+	public static Map<Double, List<Position2D>> fissilePosition2D(List<Position2D> groups
+			, double scale) {
 		Map<Double, List<Position2D>> distanceGroups= new HashMap<>();
 		Map<Double, Position2D> distanceHeart= new HashMap<>();
 		Iterator<Position2D> iterator= groups.iterator();
 		double i= 0.0;
 		Here:
-		while(iterator.hasNext()) {
-			Position2D position2D= iterator.next();
-			if(distanceGroups.isEmpty()) {
-				List<Position2D> list= new ArrayList<>();
-				list.add(position2D);
-				distanceGroups.put(i, list);
-				distanceHeart.put(i, position2D);
-			}else {
-				//遍历所有团
-				//团重心匹配如果超精度新存，不是就融入。
-				Iterator<Double> iteratorScale= distanceHeart.keySet().iterator();
-				boolean isFind= false;
-				while(iteratorScale.hasNext()) {
-					Double doubleScale= iteratorScale.next();
-					Position2D currenctHeart= distanceHeart.get(doubleScale);
-					double distance= Distance.getDistance2D(currenctHeart, position2D);
-					if(distance< scale) {
-						//融入得到新的重心
-						Position2D newHeart= Eclid.findCryptionPosition2D(currenctHeart, position2D);
-						//删除当前增加坐标集，更新坐标集
-						List<Position2D> list= distanceGroups.get(doubleScale);
-						list.add(position2D);
-						distanceGroups.put(doubleScale, list);
-						//删除当前重心数据，更新重心数据
-						distanceHeart.put(doubleScale, newHeart);
-						//找到
-						isFind= true;
-						//如果预测 坐标适应状态 可以把 continue 省略。
-						continue Here;
-					}	
-				}
-				if(!isFind) {
+			while(iterator.hasNext()) {
+				Position2D position2D= iterator.next();
+				if(distanceGroups.isEmpty()) {
 					List<Position2D> list= new ArrayList<>();
 					list.add(position2D);
-					distanceGroups.put(++i, list);
-					distanceHeart.put(i, position2D);	
+					distanceGroups.put(i, list);
+					distanceHeart.put(i, position2D);
+				}else {
+					//遍历所有团
+					//团重心匹配如果超精度新存，不是就融入。
+					Iterator<Double> iteratorScale= distanceHeart.keySet().iterator();
+					boolean isFind= false;
+					while(iteratorScale.hasNext()) {
+						Double doubleScale= iteratorScale.next();
+						Position2D currenctHeart= distanceHeart.get(doubleScale);
+						double distance= Distance.getDistance2D(currenctHeart, position2D);
+						if(distance< scale) {
+							//融入得到新的重心
+							Position2D newHeart= Eclid.findCryptionPosition2D(currenctHeart, position2D);
+							//删除当前增加坐标集，更新坐标集
+							List<Position2D> list= distanceGroups.get(doubleScale);
+							list.add(position2D);
+							distanceGroups.put(doubleScale, list);
+							//删除当前重心数据，更新重心数据
+							distanceHeart.put(doubleScale, newHeart);
+							//找到
+							isFind= true;
+							//如果预测 坐标适应状态 可以把 continue 省略。
+							continue Here;
+						}	
+					}
+					if(!isFind) {
+						List<Position2D> list= new ArrayList<>();
+						list.add(position2D);
+						distanceGroups.put(++i, list);
+						distanceHeart.put(i, position2D);	
+					}
 				}
 			}
-		}
 		return distanceGroups;	
 	}
 }
--- src/org/tinos/deta/cluster/Fusion.java
+++ src/org/tinos/deta/cluster/Fusion.java
@@ -1,4 +1,73 @@
 package org.tinos.deta.cluster;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 //task 20191220-20191222 daytime
+//通过scale 距离来进行坐标团集合 融聚。
+//Theory yaoguang.luo 20191219， 欧基里德
+//Application yaoguang.luo
 public class Fusion{
+	public static Map<Double, List<Position2D>> fusionPosition2DwithHeart
+	(Map<Double, List<Position2D>> groups, Map<Double, Position2D> groupsHeart, double scale){
+		//初始
+		Map<Double, List<Position2D>> output= new HashMap<>();
+		Map<Double, Position2D> outputHeart= new HashMap<>();
+		//逐团比较重心距离
+		Iterator<Double> outLoop= groupsHeart.keySet().iterator();
+		Iterator<Double> inLoop= groupsHeart.keySet().iterator();
+		//小于进度内融聚
+		HereOut:
+			while(outLoop.hasNext()) {
+				double out= outLoop.next();
+				HereIn:
+					while(inLoop.hasNext()) {
+						double in= inLoop.next();
+						if(out!= in) {
+							Position2D inHeart=	groupsHeart.get(in);
+							Position2D outHeart= groupsHeart.get(out);
+							double distance= Distance.getDistance2D(inHeart, outHeart);
+							if(distance< scale) {
+								//fusion
+								if(!output.containsKey(out)) {
+									if(!groups.containsKey(out)) {
+										continue HereOut;
+									}
+									if(!groups.containsKey(in)) {
+										continue HereIn;
+									}
+									List<Position2D> outList= groups.get(out);
+									List<Position2D> inList= groups.get(in);
+									Iterator<Position2D> iterator= inList.iterator();
+									while(iterator.hasNext()) {
+										outList.add(iterator.next());
+									}
+									output.put(out, outList);
+									//更新heart
+									Position2D newHeart= Eclid.findCryptionPosition2D(outHeart, inHeart);
+									outputHeart.put(out, newHeart);
+									//剔除以融聚对象
+									groups.remove(in);
+								}else {
+									List<Position2D> outList= output.get(out);
+									List<Position2D> inList= groups.get(in);
+									Iterator<Position2D> iterator= inList.iterator();
+									while(iterator.hasNext()) {
+										outList.add(iterator.next());
+									}
+									output.put(out, outList);
+									//更新heart
+									Position2D outHeartForOutput= outputHeart.get(out); 
+									Position2D newHeart= Eclid.findCryptionPosition2D(outHeartForOutput, inHeart);
+									outputHeart.put(out, newHeart);
+									//剔除以融聚对象
+									groups.remove(in);
+								}
+								groups.get(out);
+							}
+						}
+					}
+			}
+		return output;	
+	}
 } 
