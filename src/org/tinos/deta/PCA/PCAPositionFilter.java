de98f4dd80e95cff9c1ffe232436499a18626e5d
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2019-12-23 23:12:31 +0800

    增加了 小于重心距离精度内 坐标团集合筛选 算法 20191223 罗瑶光

--- src/org/tinos/deta/PCA/PCAPositionFilter.java
+++ src/org/tinos/deta/PCA/PCAPositionFilter.java
@@ -1,8 +1,11 @@
 package org.tinos.deta.PCA;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import org.tinos.deta.basic.Distance;
+import org.tinos.deta.basic.Euclid;
 import org.tinos.deta.demension.Position2D;
 import org.tinos.deta.demension.Position3D;
 //这个函数用于坐标类的 主要成份分析与提取
@@ -32,6 +35,38 @@ public class PCAPositionFilter{
 			double distance= Distance.getDistance3D(heart, position3D);
 			if(distance< scaleDistacne) {
 				output.add(position3D);
+			}
+		}
+		return output;
+	}
+	
+	public static Map<Double, List<Position2D>> filterPosition2DsWithScaledDistance(Map<Double, List<Position2D>> input
+			, Position2D heart, double scaleDistacne){
+		Map<Double, List<Position2D>> output= new HashMap<>();
+		Iterator<Double> iterator= input.keySet().iterator();
+		while(iterator.hasNext()) {
+			double mapKey= iterator.next();
+			List<Position2D> position2DList= input.get(mapKey);
+			Position2D tempHeart= Euclid.findHeartPosition2D(position2DList);
+			double distance= Distance.getDistance2D(heart, tempHeart);
+			if(distance< scaleDistacne) {
+				output.put(mapKey, position2DList);
+			}
+		}
+		return output;
+	}
+	
+	public static Map<Double, List<Position3D>> filterPosition3DsWithScaledDistance(Map<Double, List<Position3D>> input
+			, Position3D heart, double scaleDistacne){
+		Map<Double, List<Position3D>> output= new HashMap<>();
+		Iterator<Double> iterator= input.keySet().iterator();
+		while(iterator.hasNext()) {
+			double mapKey= iterator.next();
+			List<Position3D> position3DList= input.get(mapKey);
+			Position3D tempHeart= Euclid.findHeartPosition3D(position3DList);
+			double distance= Distance.getDistance3D(heart, tempHeart);
+			if(distance< scaleDistacne) {
+				output.put(mapKey, position3DList);
 			}
 		}
 		return output;
