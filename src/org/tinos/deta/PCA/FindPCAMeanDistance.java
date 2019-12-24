58bc21c45348b39c3cf0531f5a4d75b2a4d087be
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2019-12-25 03:53:02 +0800

    增加 2，3维坐标团中 每一个坐标与精度数邻接坐标集的最小距离平均值比上 整体平均值来 获取的不稳定状态观测/预测（斥力，活性）算法。 20191225 罗瑶光

--- src/org/tinos/deta/PCA/FindPCAMeanDistance.java
+++ src/org/tinos/deta/PCA/FindPCAMeanDistance.java
@@ -66,4 +66,78 @@ public class FindPCAMeanDistance{
 		}
 		return outMean/ position3Ds.size();
 	}
+	
+	//2D坐标团中 每一个坐标的斥力预测算法。
+	public static double[] findPascalMeanDistanceByEachPositions2D(List<Position2D> position2Ds
+			, double oberserverPCAScale, int sortRangeScale) {
+		double[] pascal= new double[position2Ds.size()];
+		Iterator<Position2D> outPosition2DIterator= position2Ds.iterator();
+		double outMean= 0;
+		int positionPoint=0;
+		while(outPosition2DIterator.hasNext()) {
+			Position2D outPosition2D= outPosition2DIterator.next();
+			double[] distance= new double[position2Ds.size()];
+			int i= 0;
+			Iterator<Position2D> inPosition2DIterator= position2Ds.iterator();
+			//取坐标点所有距离集合先
+			while(inPosition2DIterator.hasNext()) {
+				Position2D inPosition2D= inPosition2DIterator.next();
+				distance[i++]= Distance.getDistance2D(outPosition2D, inPosition2D);
+			}
+			//距离非对称缺陷小高峰过滤极速快排 从小到大
+			distance= new LYG4DWithDoubleQuickSort4D().sort(distance, sortRangeScale);
+			//仅仅取精度内坐标点距离求平均值，0 为本身所以从 1 开始
+			double inMean= 0;
+			for(i= 1; i<= oberserverPCAScale; i++) {
+				inMean+= distance[i];
+			}
+			inMean/= oberserverPCAScale;
+			outMean+= inMean;
+			//采集坐标点形成斥力参照的主要最短路径成份。
+			pascal[positionPoint++]= inMean;
+		}
+		outMean/= position2Ds.size();
+		for(int i= 0; i< pascal.length; i++) {
+			//比值获取 不稳定 观测数据。
+			pascal[i]/= outMean;
+		}
+		return pascal;
+	}
+	
+	//3D坐标团中 每一个坐标的斥力预测算法。
+	public static double[] findPascalMeanDistanceByEachPositions3D(List<Position3D> position3Ds
+			, double oberserverPCAScale, int sortRangeScale) {
+		double[] pascal= new double[position3Ds.size()];
+		Iterator<Position3D> outPosition3DIterator= position3Ds.iterator();
+		double outMean= 0;
+		int positionPoint=0;
+		while(outPosition3DIterator.hasNext()) {
+			Position3D outPosition3D= outPosition3DIterator.next();
+			double[] distance= new double[position3Ds.size()];
+			int i= 0;
+			Iterator<Position3D> inPosition3DIterator= position3Ds.iterator();
+			//取坐标点所有距离集合先
+			while(inPosition3DIterator.hasNext()) {
+				Position3D inPosition3D= inPosition3DIterator.next();
+				distance[i++]= Distance.getDistance3D(outPosition3D, inPosition3D);
+			}
+			//距离非对称缺陷小高峰过滤极速快排 从小到大
+			distance= new LYG4DWithDoubleQuickSort4D().sort(distance, sortRangeScale);
+			//仅仅取精度内坐标点距离求平均值，0 为本身所以从 1 开始
+			double inMean= 0;
+			for(i= 1; i<= oberserverPCAScale; i++) {
+				inMean+= distance[i];
+			}
+			inMean/= oberserverPCAScale;
+			outMean+= inMean;
+			//采集坐标点形成斥力参照的主要最短路径成份。
+			pascal[positionPoint++]= inMean;
+		}
+		outMean/= position3Ds.size();
+		for(int i= 0; i< pascal.length; i++) {
+			//比值获取 不稳定 观测数据。
+			pascal[i]/= outMean;
+		}
+		return pascal;
+	}
 }
