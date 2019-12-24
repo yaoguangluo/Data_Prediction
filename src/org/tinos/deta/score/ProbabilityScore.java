03adcbdb11607648b73d8e455b2c745e428c6105
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2019-12-24 16:33:18 +0800

    增加笛卡尔乘积 增量 评估函数 20191224 罗瑶光

--- src/org/tinos/deta/score/ProbabilityScore.java
+++ src/org/tinos/deta/score/ProbabilityScore.java
@@ -12,11 +12,30 @@ public class ProbabilityScore{
 		}
 		return sum> estimateValue? true: false;	
 	}
-	
+
 	public static boolean getEstimateSumOfScoresArray(double[] scores, double estimateValue) {
 		double sum= 0;
 		for(int i= 0; i< scores.length; i++) {
 			sum+= scores[i];
+		}
+		return sum> estimateValue? true: false;	
+	}
+	//refer µÑ¿¨¶û
+	public static boolean getEstimateCartesianSumOfScoresArray(double input, double[] EstimateRatios, double estimateValue) {
+		double sum= 0;
+		for(int i= 0; i< EstimateRatios.length; i++) {
+			sum+= EstimateRatios[i]* input;
+		}
+		return sum> estimateValue? true: false;	
+	}
+
+	//refer µÑ¿¨¶û
+	public static boolean getEstimateCartesian1DSumOfScoresArray(double[] inputs, double[] EstimateRatios, double estimateValue) {
+		double sum= 0;
+		for(int i= 0; i< EstimateRatios.length; i++) {
+			for(int j= 0; j< EstimateRatios.length; j++) {
+				sum+= inputs[i]* EstimateRatios[j];
+			}
 		}
 		return sum> estimateValue? true: false;	
 	}
