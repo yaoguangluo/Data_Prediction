b8a39aabc2291c628e6efb7d937f5b8cf2a73268
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2019-12-26 14:59:38 +0800

    增加，修正和注释一些函数 20191226 罗瑶光

--- src/org/tinos/deta/score/ProbabilityScore.java
+++ src/org/tinos/deta/score/ProbabilityScore.java
@@ -4,6 +4,7 @@ import java.util.List;
 //theory: 四则运算 
 //application: Yaoguang.Luo
 public class ProbabilityScore{
+	//该函数用于总分list评估。
 	public static boolean encodeEstimateSumOfScoresList(List<Double> scores, double estimateValue) {
 		double sum= 0;
 		Iterator<Double> iterator= scores.iterator();
@@ -12,7 +13,7 @@ public class ProbabilityScore{
 		}
 		return sum> estimateValue? true: false;	
 	}
-
+	//该函数用于总分表格评估。
 	public static boolean encodeEstimateSumOfScoresArray(double[] scores, double estimateValue) {
 		double sum= 0;
 		for(int i= 0; i< scores.length; i++) {
@@ -20,8 +21,9 @@ public class ProbabilityScore{
 		}
 		return sum> estimateValue? true: false;	
 	}
-	//refer 笛卡尔
-	public static boolean getEstimateCartesianSumOfScoresArray(double input, double[] EstimateRatios, double estimateValue) {
+	//refer 笛卡尔 该函数用于 带权值遍历和的评估。
+	public static boolean getEstimateCartesianSumOfScoresArray(double input
+			, double[] EstimateRatios, double estimateValue) {
 		double sum= 0;
 		for(int i= 0; i< EstimateRatios.length; i++) {
 			sum+= EstimateRatios[i]* input;
@@ -29,8 +31,9 @@ public class ProbabilityScore{
 		return sum> estimateValue? true: false;	
 	}
 
-	//refer 笛卡尔
-	public static boolean getEstimateCartesian1DSumOfScoresArray(double[] inputs, double[] EstimateRatios, double estimateValue) {
+	//refer 笛卡尔 该函数用于 带权值遍历笛卡尔和的评估。
+	public static boolean getEstimateCartesian1DSumOfScoresArray(double[] inputs
+			, double[] EstimateRatios, double estimateValue) {
 		double sum= 0;
 		for(int i= 0; i< EstimateRatios.length; i++) {
 			for(int j= 0; j< EstimateRatios.length; j++) {
@@ -39,8 +42,9 @@ public class ProbabilityScore{
 		}
 		return sum> estimateValue? true: false;	
 	}
-	
-	public static boolean[] encodeEstimateInitonsScore(double[] input, double[] estimateRatios, double estimateValue[]) {
+
+	public static boolean[] encodeEstimateInitonsScore(double[] input
+			, double[] estimateRatios, double estimateValue[]) {
 		boolean dnaInitons[]= new boolean[input.length];
 		for(int i= 0; i< estimateRatios.length; i++) {
 			double sum= 0;
@@ -51,13 +55,22 @@ public class ProbabilityScore{
 		}
 		return dnaInitons;
 	}  
-	
-	public static boolean[] decodeEstimateInitonsScore(double input, double[] estimateRatios, double estimateValue[]) {
+
+	public static boolean[] decodeEstimateInitonsScore(double input
+			, double[] estimateRatios, double estimateValue[]) {
 		boolean dnaInitons[]= new boolean[estimateRatios.length];
 		for(int i= 0; i< estimateRatios.length; i++) {
 			double sum= input* estimateRatios[i];
 			dnaInitons[i]= sum> estimateValue[i]? true: false;
 		}
 		return dnaInitons;
-	}  
+	} 
+	//该函数用于求数组项 各自对应的权值系数乘积 打分。
+	public static double[] getRightsEstimateScore(double[] inputs, double[] rights) {
+		double[] outputs= new double[inputs.length];
+		for(int i= 0; i< outputs.length; i++) {
+			outputs[i]= inputs[i]* rights[i];
+		}
+		return outputs;
+	}
 }
