18db9325a1a770f63ae032595b7ba7ddca7cb0ef
Lenovo LYG <Lenovo@LAPTOP-352KOFCM 313699483>
2019-12-28 05:37:29 +0800

    增加了一些误差观测函数。20191228 罗瑶光

--- src/org/tinos/deta/desolation/ErrorAsserts.java
+++ src/org/tinos/deta/desolation/ErrorAsserts.java
@@ -1,1 +1,14 @@
 package org.tinos.deta.desolation;
+public class ErrorAsserts{
+	//这个函数用于进行精度误差匹配 获取质量是否属于合格状态。
+	public boolean getErrorCaculations(double inputValue, double[] matchValues, double scale) {
+		double max= inputValue+ scale;
+		double min= inputValue- scale;
+		for(int i= 0; i< matchValues.length; i++) {
+			if(!(matchValues[i]> max|| matchValues[i]< min)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
